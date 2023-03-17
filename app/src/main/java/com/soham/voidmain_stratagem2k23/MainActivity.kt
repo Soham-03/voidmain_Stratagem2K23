fae136a9e4ba63d21e6eb17f76331ea5eab19639
Soham-03 <sohamparab1003@gmail.com>
2023-03-17 18:10:14 +0530

    Initial commit

---
+++ app/src/main/java/com/soham/voidmain_stratagem2k23/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.soham.voidmain_stratagem2k23
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
